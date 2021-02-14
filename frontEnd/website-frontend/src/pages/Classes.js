32de57ba2ea5ca30ad9a34eb9226fd70af9a6416
sonikrutik <56977066+sonikrutik@users.noreply.github.com>
2021-02-14 04:22:03 -0700

    we gave up here
    we acc gave up

--- frontEnd/website-frontend/src/pages/Classes.js
+++ frontEnd/website-frontend/src/pages/Classes.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import "../App";
+import classData from "../data/data.json";
 
 function Classes(){
     return (
         <div classname = 'Classes'>
-            <h1>Classes</h1>
+            <h1>Classes Num</h1>
+            {classData.map((postDetail, index)=>{
+                return <h1>{postDetail.containerName} - {postDetail.categoryName}({postDetail['date (DD-MM-YYYY)']}) - {postDetail.description}</h1>
+            })}
         </div>
     )
 }
-
 export default Classes;
