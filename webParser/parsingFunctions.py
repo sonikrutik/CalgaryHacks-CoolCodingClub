9d5f38e65d3b341989d6746cca75e00e6eed7864
Nibras Alam <nibraslm@gmail.com>
2021-02-13 20:53:10 -0700

    login + setup for parse
    automated login done setting up
    current task: create parsing functions

---
+++ webParser/parsingFunctions.py
@@ -0,0 +1,13 @@
+import pdfParser
+
+def parseHTMLFile(HTMLFile):
+    # pdf2Text insert later
+    # create additional functions as needed
+    # comment a shit ton
+    return
+    
+
+# for testing only
+# to login into my eclass do "python .\main.py"
+# to run this file do "python .\parsingFunctions.py"
+parseHTMLFile("parseText.html")
---
+++ webParser/pdfParser.py
@@ -0,0 +1,4 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+def pdf2Text(file):
+    return
